633ba1ff1444fed907a2c550ad2bfaf398fcecf1
Minh Trang <minhtrang@Anhs-MacBook-Air.local>
2025-01-05 21:18:16 +0700

    Day1

--- lessons/Hello.js
+++ lessons/Hello.js
@@ -1,3 +1,16 @@
 
 console.log("123");
-h
+
+
+//Variable
+//= assignment operand
+// Declaration
+let a = 1;
+
+//re-declaration is NOT allowed. 
+//reassign is acceptable
+a = 2;
+//constant
+const PI_Number = 3.14
+//Re-assign is NOT allowed. 
+PI_Number=45;

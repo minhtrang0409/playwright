59b9f066a889736a6b66af2356d60940e5452352
Minh Trang <minhtrang@Anhs-MacBook-Air.local>
2025-01-07 19:07:22 +0700

    new

--- lessons/Day_02/OperatorLearning.js
+++ lessons/Day_02/OperatorLearning.js
@@ -12,3 +12,32 @@ console.log("Total: " + (result + 1));
 // `: backstick
 console.log(`Total result: ${result} + ${1}`);
 console.log(`Total result: ${result + 1 } `);
+
+// Forward slash
+
+result = 5%2;
+console.log(`5%2: ${result}`);
+console.log(`4/2: ${4/2}`);
+console.log(`10/3: ${10/3}`);
+
+//Rounding 
+let roundingNearest = Math.round(9/2)
+console.log(`roundingNearest: ${roundingNearest}`);
+
+let roundingFloor = Math.floor(9/2)
+console.log(`roundingFloor: ${roundingFloor}`);
+
+let roundingCeil = Math.ceil(9/2)
+console.log(`roundingCeil: ${roundingCeil}`);
+
+result = 10/3
+let toFixedNumber = result.toFixed(3)
+console.log(`toFixedNumber: ${toFixedNumber}`);
+
+let multi = 3**2;
+console.log(`multi: ${multi}`);
+
+
+
+
+
